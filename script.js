27e500e133aeb1e6ca45d310629e36f06c1bd4f2
lohanbilik <lohan.bilik@escola.pr.gov.br>
2026-06-15 12:16:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function insert (num){
+    var numero = document.getElementById ('resultado'). innerHTML;
+    document.getElementById('resultado').innerHTML=numero+num;
+}
+function clean (){
+    document.getElementById('resultado').innerHTML="";
+}
+function back(){
+    var numero = document.getElementById ('resultado'). innerHTML;
+    document.getElementById('resultado').innerHTML=resultado.substring(0,resultado.lenght-1);
+}
